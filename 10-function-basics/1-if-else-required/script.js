ca7a2366f306efe0f1e58b37e14ea234b49e5a01
belmehd <mebefrint@gmail.com>
2021-10-15 12:25:34 +0200

    commit exercices : function-basics

--- 10-function-basics/1-if-else-required/script.js
+++ 10-function-basics/1-if-else-required/script.js
@@ -2,3 +2,6 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+/*
+il n'y a aucune différence car si 'return true' est false il executera 'return confirm'
+*/
--- 10-function-basics/4-pow/script.js
+++ 10-function-basics/4-pow/script.js
@@ -2,3 +2,20 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+
+function pow(x, n) {
+      let result = x;
+      for (let i = 1; i < n; i++) {
+            result *= x // result = à result * x
+      }
+      return result;
+}
+
+let x = +prompt("quels nombres x ?", '');
+let n = +prompt("quels nombres n ?", '');
+
+if (n < 1) {
+      alert(`power ${n} is not supported, use a positive integer`);
+}else{
+      alert(pow(x, n));
+}
