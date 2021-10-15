52355497e5e2aa8dc4118bb762e47a1c8c06eb9f
belmehd <mebefrint@gmail.com>
2021-10-15 11:22:43 +0200

    commit exercices : type-convertions et switch

--- 08-type-conversions/script.js
+++ 08-type-conversions/script.js
@@ -2,3 +2,16 @@
       - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
       - N'oubliez pas de sauvegarder pour afficher les changements.
 */
+let value = true;
+alert(typeof value);
+value = String(value)
+
+let str = "123";
+alert(typeof str);
+str = Number(str)
+
+let nb = 1;
+alert(typeof nb);
+nb = Boolean(nb)
+nb = 1;
+nb = 0;
